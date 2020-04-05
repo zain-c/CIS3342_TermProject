da469cd2c35c00f71a9a6e4a0ec59d0b907fe8ad
Zain Contractor <tug85523@temple.edu>
2020-04-05 15:09:50 -0400

    Added Term Project Solution

---
+++ TermProject/DatingSiteLibrary/User.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatingSiteLibrary
+{
+    public class User
+    {
+        string username;
+        string email;
+        string firstName;
+        string lastName;
+        string homeAddress;
+        string homeCity;
+        string homeState;
+        int homeZip;
+        string billingAddress;
+        string billingCity;
+        string billingState;
+        int billingZip;
+        string securityQuestion1;
+        string securityQuestion2;
+        string securityQuestion3;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+    }
+}
---
+++ TermProject/TermProject/Registration.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Utilities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TermProject
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
