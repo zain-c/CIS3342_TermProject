fc4ae0a2c818f1fd7c542807e7a0b9712034e653
Zain Contractor <tug85523@temple.edu>
2020-04-15 12:52:37 -0400

    edited login page style

--- TermProject/TermProject/Login.aspx.cs
+++ TermProject/TermProject/Login.aspx.cs
@@ -11,7 +11,7 @@ namespace TermProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
