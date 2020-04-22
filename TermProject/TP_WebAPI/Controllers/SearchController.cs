005868f44b808d551f47001f30a6c4313d26e722
aderbs7 <58049624+aderbs7@users.noreply.github.com>
2020-04-22 11:51:07 -0400

    Add search contoller and search methods

---
+++ TermProject/TP_WebAPI/Controllers/SearchController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Utilities;
+using DatingSiteLibrary;
+
+namespace TP_WebAPI.Controllers
+{
+    [Route("api/DatingService/Search")]
+    [ApiController]
+    public class SearchController : Controller
+    {
+        //searchParameters are passed in as a bar (|) delimited string. Search fields are 
+        //ordered from left to right, top to bottom.
+
+
+        [HttpGet("LoadSearchResults/Member")]
+        public List<UserProfile> loadMemberSearch(string searchParameters)
+        {
+            //Parameters: City|State|Gender|Commitment|Have Kids|Want Kids|Interests
+            List<UserProfile> profiles = new List<UserProfile>();
+
+            string[] searchArray = searchParameters.Split('|');
+            User tempUser = new User();
+            int userID = tempUser.getUserID(username);
+
+            UserProfile profile = new UserProfile();
+             
+
+            return profiles;
+        }
+
+        [HttpGet("LoadSearchResults/Nonmember")]
+        public UserProfile loadNonmemberSearch(string searchParameters)
+        {
+            //Parameters: City|State|Gender
+
+            string[] searchArray = searchParameters.Split('|');
+            User tempUser = new User();
+            int userID = tempUser.getUserID(username);
+
+            UserProfile profile = new UserProfile();
+
+            return profile.retreiveUserProfileFromDB(userID);
+        }
+    }
+}
--- TermProject/TermProject/Profile.aspx.cs
+++ TermProject/TermProject/Profile.aspx.cs
@@ -24,17 +24,17 @@ namespace TermProject
             }
             else
             {
-                loadProfile(Session["RequestedProfile"].ToString());
+                //loadProfile(Session["RequestedProfile"].ToString());
             }
             
         }
         
-        private void loadProfile(string username)
+        /*private void loadProfile(string username)
         {
             string url = "http://cis-iis2.temple.edu/Spring2020/CIS3342_tug85523/WebAPITest/api/DatingService/Profiles/LoadUserProfile/" + username;
 
             WebRequest request = WebRequest.Create(url);
             we
-        }
+        }*/
     }
 }
