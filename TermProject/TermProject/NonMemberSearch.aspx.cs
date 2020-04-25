0b10ee97d959c7433c57fef35c7e062d911d2527
aderbs7 <58049624+aderbs7@users.noreply.github.com>
2020-04-25 11:06:30 -0400

    Commit

--- TermProject/TermProject/NonMemberSearch.aspx.cs
+++ TermProject/TermProject/NonMemberSearch.aspx.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using Utilities;
+using DatingSiteLibrary;
+using System.Web.Script.Serialization;  // needed for JSON serializers
+using System.IO;                        // needed for Stream and Stream Reader
+using System.Net;                       // needed for the Web Request
+using System.Drawing.Imaging;
+
 
 namespace TermProject
 {
@@ -16,12 +25,15 @@ namespace TermProject
 
         protected void btnSearch_Click(object sender, EventArgs e)
         {
-            
+            if (validateFields())
+            {
+                loadResults(txtLocationFilter.Text, ddStateFilter.SelectedValue, ddGenderFilter.SelectedValue);
+            }
 
 
         }
 
-        protected void validateFields()
+        protected bool validateFields()
         {
             bool valid = true;
             if (string.IsNullOrWhiteSpace(txtLocationFilter.Text) || (txtLocationFilter.Text.Length > 50))
@@ -42,6 +54,28 @@ namespace TermProject
                 lblErrorMsg.Text += "*Please select a gender preference. <br />";
                 lblErrorMsg.Visible = true;
             }
+
+            return valid;
+        }
+
+        private void loadResults(string city, string state, string gender)
+        {
+            string url = "https://localhost:44369/api/DatingService/Profiles/LoadUserProfile/" + city + "/" + state + "/" + gender;
+
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            Stream theDataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(theDataStream);
+            string data = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            UserProfile[] profileResults = js.Deserialize<UserProfile[]>(data);
+
+            gvSearchResults.DataSource = profileResults;
+            gvSearchResults.DataBind();
         }
     }
 }
