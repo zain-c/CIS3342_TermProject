51e5a57d45eb462b5723522e3a8d0ad847f5f24e
Zain Contractor <tug85523@temple.edu>
2020-04-25 21:12:15 -0400

    added some comments

--- TermProject/TermProject/LikedPassList.aspx.cs
+++ TermProject/TermProject/LikedPassList.aspx.cs
@@ -232,8 +232,9 @@ namespace TermProject
         {
             string rowClicked = ((Button)sender).ID.Split('_')[1];
             ProfileDisplay profileDisplay = ((ProfileDisplay)passes.FindControl("pdProfilePass_" + rowClicked));
-            string usernameToTransfer = profileDisplay.Username;
+            string usernameToTransfer = profileDisplay.Username; //get the username of the profile to remove from Passes and add to Likes
 
+            //First remove the profile from Passes List
             string removeUrl = "https://localhost:44369/api/DatingService/Profiles/RemoveFromPasses/" + Session["Username"].ToString() + "/" + usernameToTransfer;
             WebRequest removeRequest = WebRequest.Create(removeUrl);
             removeRequest.Method = "DELETE";
@@ -246,6 +247,7 @@ namespace TermProject
             removeReader.Close();
             removeResponse.Close();
 
+            //Now add that profile to Likes List
             string likeUrl = "https://localhost:44369/api/DatingService/Profiles/AddLikes/" + Session["Username"].ToString() + "/" + usernameToTransfer;
             WebRequest likeRequest = WebRequest.Create(likeUrl);
             likeRequest.Method = "PUT";
@@ -257,14 +259,19 @@ namespace TermProject
             string likeData = likeReader.ReadToEnd();
             likeReader.Close();
             likeResponse.Close();
-
-            if(removeData == "true" && likeData == "1")
+                        
+            if(removeData == "true" && likeData == "1") //if the transfer works, then reload Likes and Passes Lists
             {
                 likes.Controls.RemoveAt(likes.Controls.Count - 1);
                 loadLikes();
 
                 passes.Controls.RemoveAt(passes.Controls.Count - 1);
                 loadPasses();
+            }
+            else
+            {
+                lblErrorMsg.Text += "*There was an error moving this user to your Likes. <br />";
+                lblErrorMsg.Visible = true;
             }
         }
 
